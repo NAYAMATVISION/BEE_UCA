2b2469b85dbadc4b69b822d93a94fd6def8586f4
NAYAMATVISION <nayamatemeet@gmail.com>
2026-08-06 20:18:24 +0530

    added fibonaci generator function

--- week_5/index.js
+++ week_5/index.js
@@ -1,51 +1,76 @@
-// calculator using generator function
-function *Calculator(){
+// // calculator using generator function
+// function *Calculator(){
    
-    while(true){
-        try {
-             const instruction = yield "next calculation";
+//     while(true){
+//         try {
+//              const instruction = yield "next calculation";
 
-            const {num1, num2 , operator} = instruction;
+//             const {num1, num2 , operator} = instruction;
 
-            if(typeof num1!='number' || typeof num2!= 'number') throw new Error("Type error : inputs must be numbers");
+//             if(typeof num1!='number' || typeof num2!= 'number') throw new Error("Type error : inputs must be numbers");
 
-            let result;
-            switch (operator) {
-                case '+': result = num1+num2;
-                    break;
-                case '-': result = num1-num2;
-                    break;
-                case '*' : result = num1*num2;
-                    break;
-                case '/': 
-                    if(num2==0) throw new Error("denominator can not be zero");
-                    num1/num2;
-                    break;
+//             let result;
+//             switch (operator) {
+//                 case '+': result = num1+num2;
+//                     break;
+//                 case '-': result = num1-num2;
+//                     break;
+//                 case '*' : result = num1*num2;
+//                     break;
+//                 case '/': 
+//                     if(num2==0) throw new Error("denominator can not be zero");
+//                     num1/num2;
+//                     break;
             
-                default:
-                    throw new Error("Invalid operator");
-                    break;
-            }
-               yield result; 
-        } catch (error) {
-            yield error.message;
-        }
+//                 default:
+//                     throw new Error("Invalid operator");
+//                     break;
+//             }
+//                yield result; 
+//         } catch (error) {
+//             yield error.message;
+//         }
+//     }
+// }
+
+// const calc = Calculator();
+// calc.next();
+
+// const op1 = calc.next({num1:23 , num2 : 87 , operator : '*'});
+// console.log(op1.value);
+// calc.next();
+
+// const op2= calc.next({num1 : 9 , num2 : "4" , operator : '+'});
+// console.log(op2.value);
+// calc.next();
+
+// const op3 = calc.next({num1 : 23 , num2 : 0 , operator : '/'});
+// console.log(op3.value);
+
+
+function *fibonacci(){
+    let a = 0;
+    let b = 1;
+
+    while(true){
+        yield a;
+
+        let temp = a;
+        a = b;
+        b = a + temp;
     }
+    
 }
+const fib = fibonacci();
 
-const calc = Calculator();
-calc.next();
-
-const op1 = calc.next({num1:23 , num2 : 87 , operator : '*'});
-console.log(op1.value);
-calc.next();
-
-const op2= calc.next({num1 : 9 , num2 : "4" , operator : '+'});
-console.log(op2.value);
-calc.next();
-
-const op3 = calc.next({num1 : 23 , num2 : 0 , operator : '/'});
-console.log(op3.value);
+ console.log(fib.next().value);
+    console.log(fib.next().value);
+    console.log(fib.next().value);
+    console.log(fib.next().value);
+    console.log(fib.next().value);
+    console.log(fib.next().value);
+    console.log(fib.next().value);
+    console.log(fib.next().value);
 
 
 
