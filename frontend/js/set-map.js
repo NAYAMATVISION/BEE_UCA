1dbf36025a7dbf2bd2cc1d7351802c974e87c96f
NAYAMATVISION <nayamatemeet@gmail.com>
2026-08-08 10:34:07 +0530

    added set.maps js and started OOP IN JS

---
+++ frontend/js/set-map.js
@@ -0,0 +1,10 @@
+let map1 = new Map();
+map1.set("name", "John");
+map1.set("age", 30);
+map1.set("city", "New York");
+map1.set("country", "USA");
+console.log(map1.get("name"));
+
+
+// set is used for maintaining unique values in a collection. It does not allow duplicate values.
+// map is used for lookups 
