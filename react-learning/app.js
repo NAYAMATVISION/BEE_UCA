8630f991e0db68f3d1288c07e515154ba50c7d96
NAYAMATVISION <nayamatemeet@gmail.com>
2026-09-11 10:30:53 +0530

    started react JS

---
+++ react-learning/app.js
@@ -0,0 +1,20 @@
+// const app = document.getElementById("reactapp");
+
+// const root = ReactDOM.createRoot(app);
+
+// root.render(
+//     <h1>Hello React</h1>
+// );
+
+// function App(){
+//     return <h1>Hello React</h1>;
+// }
+
+// const app = document.getElementById("reactapp");
+
+// const root = ReactDOM.createRoot(app);
+
+// root.render(
+// <App />
+// );
+
